9c46b8008d9a0ed8ddc82d9d908d15ccd728c7e5
Kahli Holmes <kahliholmes@utexas.edu>
2014-05-24 12:06:38 -0500

    finished p1

--- problem01.java
+++ problem01.java
@@ -1,1 +1,15 @@
 /* Incomplete Implementaion */
+public class problem01{
+public static void main(String args[])
+{
+	int sum = 0;
+	for(int i=0; i<1000; i++)
+	{
+		if((i%3 == 0) || (i%5 ==0))
+		{
+			sum+=i;
+		}
+	}
+	System.out.println(sum);
+}
+}
